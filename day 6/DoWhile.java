5e225c19bf5d4040c704ecb0907138f085003f60
GauravTiwari09 <114760309+GauravTiwari09@users.noreply.github.com>
2022-10-07 20:17:15 +0530

    Create DoWhile.java

---
+++ day 6/DoWhile.java
@@ -0,0 +1,10 @@
+public class Do {
+    public static void main(String[] args) {
+        int i=1;
+        do {
+            System.out.println("Work hard success will be yours");
+            i++;
+        }
+        while (i<=10);
+        }
+    }
