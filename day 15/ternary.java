79265d172c248c58b2c351b7878161e7c1af552c
GauravTiwari09 <114760309+GauravTiwari09@users.noreply.github.com>
2022-11-11 00:22:17 +0530

    Create ternary.java

---
+++ day 15/ternary.java
@@ -0,0 +1,10 @@
+
+
+public class PassorFail {
+    public static void main(String[] args) {
+        int marks = 33;
+        String type = (marks>=33)? "PASS": "FAIL";
+        System.out.println(type);
+
+    }
+}
